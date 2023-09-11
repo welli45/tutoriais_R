90abdbeb1fdf753ce31d2a3caec1680b9c2a5cb2
Wellington Santos Souza <wellington.santos.souza45@gmail.com>
2023-09-11 15:17:26 -0300

    in

---
+++ python_Basico.py
@@ -0,0 +1,83 @@
+################################################################################
+## Wellington Santos Souza
+## 11/09/23
+################################################################################
+
+# Python Básico
+
+# Tipos de variáveis 
+# variáveis são pedacinhos na memória do computador onde guardamos as informações
+
+# inteiro
+x = -10
+type(x) 
+# real
+y = 3.14
+type(y)
+# texto
+nome = 'Wellington'
+type(nome)
+# lógica
+a = y == x
+type(a)
+
+# Operadores aritméticos
+
+# Podemos fazer operações aritméticas simples
+a = 2 + 3  # Soma
+b = 2 - 3  # Subtração
+c = 2 * 3  # Multiplicação
+d = 2 / 3  # Divisão
+e = 2 // 3 # Divisão inteira
+f = 2 ** 3 # Potência
+g = 2 % 3  # Resto de divisão
+
+print (a, b, c, d, e, f, g)
+
+# podemos fazer operações dentro do print
+print(a+b)
+
+# podemos fazer operações com variáveis não numéricas
+
+Nome = input("Digite seu nome:")
+
+Nome = Nome + ' Santos Souza'
+
+print(Nome)
+
+# Operadores relacionais
+
+comp = 5 > 6
+print(comp)
+
+comp01 = 6==6
+print(comp01)
+
+# O Python possui 6 operadores relacionais:
+# Maior que: >
+# Maior ou igual: >=
+# Menor que: <
+# Menor ou igual: <=
+# Igual: ==
+# Diferente: !=
+
+# Operadores lógicos
+
+# and: verdadeiro se condição 1 for verdadeira e condição 2 for verdadeira
+# or: verdadeiro se condição 1 for verdadeira ou condição 2 for verdadeira
+
+comp = 5 > 4 and 4 == (2*2)
+print(comp)
+
+comp01 = 
+
+
+
+
+
+
+
+
+
+
+
